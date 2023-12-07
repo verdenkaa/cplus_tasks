0f0d54163cd78fc0b06480b2c1ad77f280bdbb66
verdenkaa <nikitapahomov97@gmail.com>
2023-12-07 17:45:06 +0300

    Update 8.cpp

--- 8.cpp
+++ 8.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int count = 0;
-    
-
-    return 0;
+int main()
+{
+	int NUM = 0;
+	int a, k, a2;
+	cout << "Количество наборов: "; 
+	cin >> k;
+	cout << endl << "Наборы чисел разделенные 0 (в конце тоже): ";
+	for (int i = 0; i < k; i++){
+		bool t = true;
+		cin >> a;
+		while(a != 0){
+			cin >> a2;
+			if (a2 < a && a2 != 0){
+			t = false;
+			break;}
+			a = a2;
+		}
+		if (t){NUM++;}
+		}
+	cout <<endl << "Количество наборов: " << NUM;
 }
